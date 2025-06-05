db31be46828d5e61c6747bcc450e92f4cde7022a
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-06-06 00:38:49 +0530

    added unique constraint to user_id also

--- sql/schema/004_refresh_token.sql
+++ sql/schema/004_refresh_token.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 create table refresh_token(
     token text not null unique,
-    user_id uuid not null references users(id) on delete cascade,
+    user_id uuid unique not null references users(id) on delete cascade,
     created_at timestamp not null,
     expires_at timestamp not null
 );
