fb8d211d58755c94174f8ebdff8a00381a814747
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-06-10 15:51:19 +0530

    re-designed

--- event_handlers/message.event_handlers.go
+++ event_handlers/message.event_handlers.go
@@ -6,44 +6,144 @@ import (
 	"sync"
 	"time"
 
-	"github.com/harshvardha/TerTerChat/internal/database"
+	"github.com/google/uuid"
 	"github.com/harshvardha/TerTerChat/internal/services"
+)
+
+type Message struct {
+	ID              uuid.UUID
+	Description     string
+	SenderID        uuid.UUID
+	ReceiverID      uuid.UUID
+	GroupID         uuid.UUID
+	SenderUsername  string
+	GroupMemberID   uuid.UUID
+	GroupMemberName string
+	CreatedAt       string
+	UpdatedAt       string
+}
+
+// Message data for NEW_MESSAGE | EDIT_MESSAGE event
+type newOrEditMessage struct {
+	ID             uuid.UUID `json:"id"`
+	GroupID        uuid.UUID `json:"group_id,omitempty"`
+	SenderID       uuid.UUID `json:"sender_id"`
+	SenderUsername string    `json:"sender_username,omitempty"`
+	Description    string    `json:"description"`
+	CreatedAt      string    `json:"created_at,omitempty"`
+	UpdatedAt      string    `json:"updated_at,omitempty"`
+}
+
+// Message data for DELETE_MESSAGE event
+type deleteMessage struct {
+	ID       uuid.UUID `json:"id"`
+	SenderID uuid.UUID `json:"sender_id"`
+	GroupID  uuid.UUID `json:"group_id,omitempty"`
+}
+
+// Message data for MESSAGE_RECEIVED event
+type markMessageReceived struct {
+	ID         uuid.UUID `json:"id"`
+	ReceiverID uuid.UUID `json:"receiver_id"`
+}
+
+// Message data for GROUP_MESSAGE_READ event
+type markGroupMessageRead struct {
+	ID                  uuid.UUID `json:"id"`
+	GroupID             uuid.UUID `json:"group_id"`
+	GroupMemberID       uuid.UUID `json:"group_member_id"`
+	GroupMemberUsername string    `json:"group_member_username"`
+}
+
+const (
+	NEW_MESSAGE        = "NEW_MESSAGE"
+	EDIT_MESSAGE       = "EDIT_MESSAGE"
+	DELETE_MESSAGE     = "DELETE_MESSAGE"
+	MESSAGE_RECEIVED   = "MARK_MESSAGE_RECEIVED"
+	GROUP_MESSAGE_READ = "GROUP_MESSAGE_READ"
 )
 
 type MessageEvent struct {
 	Name                string
 	Phonenumbers        []string
-	Message             database.Message
+	Message             Message
 	NotificationService *services.Notification
 	EmittedAt           time.Time
 }
-
-type message struct {
-	Message     database.Message `json:"message,omitempty"`
-	MessageType string           `json:"messageType"`
-}
-
-const (
-	NEW_MESSAGE    = "NEW_MESSAGE"
-	EDIT_MESSAGE   = "EDIT_MESSAGE"
-	DELETE_MESSAGE = "DELETE_MESSAGE"
-)
 
 func MessageEventHandler(event chan MessageEvent, wg *sync.WaitGroup) {
 	defer wg.Done()
 	log.Printf("[EVENT_HANDLER]: message event handler started")
 	for messageEvent := range event {
 		log.Printf("[EVENT]: %s, [TIME]: %s", messageEvent.Name, messageEvent.EmittedAt.Format(time.RFC1123))
-		msg, err := json.Marshal(message{
-			Message:     messageEvent.Message,
-			MessageType: messageEvent.Name,
-		})
-		if err != nil {
-			log.Printf("[EVENT]: Unable to marshal message: %v", err)
-			return
+		switch messageEvent.Name {
+		case NEW_MESSAGE:
+			msg, err := json.Marshal(newOrEditMessage{
+				ID:             messageEvent.Message.ID,
+				GroupID:        messageEvent.Message.GroupID,
+				SenderID:       messageEvent.Message.SenderID,
+				SenderUsername: messageEvent.Message.SenderUsername,
+				Description:    messageEvent.Message.Description,
+				CreatedAt:      messageEvent.Message.CreatedAt,
+			})
+			if err != nil {
+				log.Printf("[MESSAGE_EVENT_HANDLER]: error marshalling json for new_message: %v", err)
+				continue
+			}
+
+			go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
+		case EDIT_MESSAGE:
+			msg, err := json.Marshal(newOrEditMessage{
+				ID:          messageEvent.Message.ID,
+				GroupID:     messageEvent.Message.GroupID,
+				SenderID:    messageEvent.Message.SenderID,
+				Description: messageEvent.Message.Description,
+				UpdatedAt:   messageEvent.Message.UpdatedAt,
+			})
+			if err != nil {
+				log.Printf("[MESSAGE_EVENT_HANDLER]: error marshalling json for edit_message: %v", err)
+				continue
+			}
+
+			go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
+		case DELETE_MESSAGE:
+			msg, err := json.Marshal(deleteMessage{
+				ID:       messageEvent.Message.ID,
+				SenderID: messageEvent.Message.SenderID,
+				GroupID:  messageEvent.Message.GroupID,
+			})
+			if err != nil {
+				log.Printf("[MESSAGE_EVENT_HANDLER]: error marshalling json for delete_message: %v", err)
+				continue
+			}
+
+			go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
+		case MESSAGE_RECEIVED:
+			msg, err := json.Marshal(markMessageReceived{
+				ID:         messageEvent.Message.ID,
+				ReceiverID: messageEvent.Message.ReceiverID,
+			})
+			if err != nil {
+				log.Printf("[MESSAGE_EVENT_HANDLER]: error marshalling json for message_received: %v", err)
+				continue
+			}
+
+			go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
+		case GROUP_MESSAGE_READ:
+			msg, err := json.Marshal(markGroupMessageRead{
+				ID:                  messageEvent.Message.ID,
+				GroupID:             messageEvent.Message.GroupID,
+				GroupMemberID:       messageEvent.Message.GroupMemberID,
+				GroupMemberUsername: messageEvent.Message.GroupMemberName,
+			})
+			if err != nil {
+				log.Printf("[MESSAGE_EVENT_HANDLER]: error marshalling json for group_message_read: %v", err)
+				continue
+			}
+
+			go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
 		}
 
-		go messageEvent.NotificationService.PushNotification(messageEvent.Phonenumbers, msg)
 	}
 
 	log.Printf("[EVENT]: Message event handler for %v stopped because event channel was closed", (<-event).Phonenumbers)
