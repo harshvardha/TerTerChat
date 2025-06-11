46e2dfa006ffd796ddab213d8162ebaa073085b3
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-06-11 22:11:34 +0530

    completed

--- controllers/group.controller.go
+++ controllers/group.controller.go
@@ -1,31 +1,535 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 
-func (apiConfig *ApiConfig) CreateGroup(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+	"github.com/google/uuid"
+	eventhandlers "github.com/harshvardha/TerTerChat/event_handlers"
+	"github.com/harshvardha/TerTerChat/internal/database"
+	"github.com/harshvardha/TerTerChat/utility"
+)
 
+func (apiConfig *ApiConfig) CreateGroup(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		Name string `json:"name"`
+	}
+
+	type response struct {
+		ID          uuid.UUID `json:"id"`
+		Name        string    `json:"name"`
+		CreatedAt   string    `json:"created_at"`
+		AccessToken string    `json:"access_token"`
+	}
+
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/create]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if len(params.Name) == 0 {
+		log.Printf("[/api/v1/group/create]: empty name field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty name field")
+		return
+	}
+
+	// creating group and making the requesting user admin
+	newGroup, err := apiConfig.DB.CreateGroup(r.Context(), params.Name)
+	if err != nil {
+		log.Printf("[/api/v1/group/create]: error creating new group: %v", err)
+		utility.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err = apiConfig.DB.MakeUserAdmin(r.Context(), database.MakeUserAdminParams{
+		UserID:  userID,
+		GroupID: newGroup.ID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/create]: error making the requesting user admin: %v", err)
+		if removeGroupErr := apiConfig.DB.DeleteGroup(r.Context(), newGroup.ID); removeGroupErr != nil {
+			log.Printf("[/api/v1/group/create]: error removing group: %v", err)
+		}
+
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utility.RespondWithJson(w, http.StatusCreated, response{
+		ID:          newGroup.ID,
+		Name:        newGroup.Name,
+		CreatedAt:   newGroup.CreatedAt.Format(time.RFC1123),
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) UpdateGroupName(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) UpdateGroupName(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		GroupID uuid.UUID `json:"group_id"`
+		Name    string    `json:"name"`
+	}
 
+	type response struct {
+		Name        string `json:"name"`
+		UpdatedAt   string `json:"updated_at"`
+		AccessToken string `json:"access_token"`
+	}
+
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/update]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if len(params.Name) == 0 {
+		log.Printf("[/api/v1/group/update]: empty name field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty name field")
+		return
+	}
+
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/update]: empty group id")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id")
+		return
+	}
+
+	// checking if the requesting user id admin of group or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/update]: requesting user is not group admin: %v", err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// updating group name
+	updatedGroup, err := apiConfig.DB.UpdateGroup(r.Context(), database.UpdateGroupParams{
+		Name: params.Name,
+		ID:   params.GroupID,
+	})
+	if err != nil {
+		log.Printf("[/api/v1/group/update]: error updating group name: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utility.RespondWithJson(w, http.StatusOK, response{
+		Name:        updatedGroup.Name,
+		UpdatedAt:   updatedGroup.UpdatedAt.Format(time.RFC1123),
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) RemoveGroup(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) RemoveGroup(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		GroupID uuid.UUID `json:"group_id"`
+	}
 
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/remove]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/remove]: empty group id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id field")
+		return
+	}
+
+	// checking if the requesting user id admin or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/remove]: requesting user %s is not admin: %v", userID, err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// removing group
+	if err = apiConfig.DB.DeleteGroup(r.Context(), params.GroupID); err != nil {
+		log.Printf("[/api/v1/group/remove]: error removing group: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utility.RespondWithJson(w, http.StatusOK, EmptyResponse{
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) AddUserToGroup(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) AddUserToGroup(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		UserID  uuid.UUID `json:"user_id"`
+		GroupID uuid.UUID `json:"group_id"`
+	}
 
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/add]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if params.UserID == uuid.Nil {
+		log.Printf("[/api/v1/group/user/add]: empty user id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty user id field")
+		return
+	}
+
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/user/remove]: empty group id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id field")
+		return
+	}
+
+	// checking if the requesting user id admin or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/user/add]: requesting user is not admin: %v", err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// adding user to group
+	if err = apiConfig.DB.AddUserToGroup(r.Context(), database.AddUserToGroupParams{
+		UserID:  params.UserID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/user/add]: error adding user to group: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// emit group event ADD_USER_TO_GROUP
+	groupEvent := eventhandlers.GroupEvent{}
+	groupEvent.Name = eventhandlers.ADD_USER_TO_GROUP
+
+	// creating group actions
+	user, err := apiConfig.DB.GetUserById(r.Context(), params.UserID)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/add]: error fetching requested user information: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Group = eventhandlers.Group{
+		ID:          params.GroupID,
+		Username:    user.Username,
+		Phonenumber: user.Phonenumber,
+	}
+
+	// fetching group members phonenumbers
+	phonenumbers, err := apiConfig.DB.GetGroupMembersPhonenumbers(r.Context(), params.GroupID)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/remove]: error fetching phonenumbers of group members: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Phonenumbers = phonenumbers
+
+	// adding notification service
+	groupEvent.NotificationService = apiConfig.NotificationService
+
+	// adding event emitting time
+	groupEvent.EmittedAt = time.Now()
+
+	// emitting event
+	apiConfig.GroupActionsEventEmitterChannel <- groupEvent
+
+	utility.RespondWithJson(w, http.StatusOK, EmptyResponse{
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) RemoveUserFromGroup(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) RemoveUserFromGroup(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		UserID  uuid.UUID `json:"user_id"`
+		GroupID uuid.UUID `json:"group_id"`
+	}
 
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/remove]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if params.UserID == uuid.Nil {
+		log.Printf("[/api/v1/group/user/remove]: empty user id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty user id field")
+		return
+	}
+
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/user/remove]: empty group id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id field")
+		return
+	}
+
+	// checking if the requesting user id admin or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/user/remove]: requesting user is not admin: %v", err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// removing user from group
+	if err = apiConfig.DB.RemoveUserFromGroup(r.Context(), database.RemoveUserFromGroupParams{
+		UserID:  params.UserID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/user/remove]: error removing user from group: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// emit group event REMOVE_USER_FROM_GROUP
+	groupEvent := eventhandlers.GroupEvent{}
+	groupEvent.Name = eventhandlers.REMOVE_USER_FROM_GROUP
+
+	// creating group action
+	user, err := apiConfig.DB.GetUserById(r.Context(), params.UserID)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/remove]: error fetching requested user information: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Group = eventhandlers.Group{
+		ID:          params.GroupID,
+		Username:    user.Username,
+		Phonenumber: user.Phonenumber,
+	}
+
+	// fetching group members phonenumbers
+	phonenumbers, err := apiConfig.DB.GetGroupMembersPhonenumbers(r.Context(), params.GroupID)
+	if err != nil {
+		log.Printf("[/api/v1/group/user/remove]: error fetching phonenumbers of group members: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Phonenumbers = phonenumbers
+
+	// adding notification service
+	groupEvent.NotificationService = apiConfig.NotificationService
+
+	// adding event emitting time
+	groupEvent.EmittedAt = time.Now()
+
+	// emitting event
+	apiConfig.GroupActionsEventEmitterChannel <- groupEvent
+
+	utility.RespondWithJson(w, http.StatusOK, EmptyResponse{
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) MakeUserAdmin(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) MakeUserAdmin(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		GroupID uuid.UUID `json:"group_id"`
+		UserID  uuid.UUID `json:"user_id"`
+	}
 
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/make]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/admin/make]: empty group id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id field")
+		return
+	}
+
+	if params.UserID == uuid.Nil {
+		log.Printf("[/api/v1/group/admin/make]: empty user id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty user id field")
+		return
+	}
+
+	// checking if the requesting user is admin or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/admin/make]: requesting user is not admin: %v", err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// making user admin
+	if err = apiConfig.DB.MakeUserAdmin(r.Context(), database.MakeUserAdminParams{
+		GroupID: params.GroupID,
+		UserID:  params.UserID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/admin/make]: error making user admin: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// emit group event MADE_ADMIN
+	groupEvent := eventhandlers.GroupEvent{}
+	groupEvent.Name = eventhandlers.MADE_ADMIN
+
+	// creating group action
+	user, err := apiConfig.DB.GetUserById(r.Context(), params.UserID)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/make]: error fetching requested user information: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Group = eventhandlers.Group{
+		ID:          params.GroupID,
+		Username:    user.Username,
+		Phonenumber: user.Phonenumber,
+	}
+
+	// fetching phonenumbers of group members
+	phonenumbers, err := apiConfig.DB.GetGroupMembersPhonenumbers(r.Context(), params.GroupID)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/make]: error fetching phonenumbers for group members: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Phonenumbers = phonenumbers
+
+	// adding notification service
+	groupEvent.NotificationService = apiConfig.NotificationService
+
+	// adding event emitting time
+	groupEvent.EmittedAt = time.Now()
+
+	// emitting event
+	apiConfig.GroupActionsEventEmitterChannel <- groupEvent
+
+	utility.RespondWithJson(w, http.StatusOK, EmptyResponse{
+		AccessToken: newAccessToken,
+	})
 }
 
-func (apiConfig *ApiConfig) RemoveUserFromAdmin(w http.ResponseWriter, r *http.Request, userID string, newAccessToken string) {
+func (apiConfig *ApiConfig) RemoveUserFromAdmin(w http.ResponseWriter, r *http.Request, userID uuid.UUID, newAccessToken string) {
+	type request struct {
+		UserID  uuid.UUID `json:"user_id"`
+		GroupID uuid.UUID `json:"group_id"`
+	}
 
+	// extracting request body
+	decoder := json.NewDecoder(r.Body)
+	params := request{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/remove]: error decoding request body: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// validating request body
+	if params.UserID == uuid.Nil {
+		log.Printf("[/api/v1/group/admin/remove]: empty user id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty user id field")
+		return
+	}
+
+	if params.GroupID == uuid.Nil {
+		log.Printf("[/api/v1/group/admin/remove]: empty group id field")
+		utility.RespondWithError(w, http.StatusNotAcceptable, "empty group id field")
+		return
+	}
+
+	// checking if the requesting user is admin or not
+	if _, err := apiConfig.DB.IsUserAdmin(r.Context(), database.IsUserAdminParams{
+		UserID:  userID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/admin/remove]: requesting user is not admin: %v", err)
+		utility.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	// removing requested user from admin
+	if err = apiConfig.DB.RemoveUserFromAdmin(r.Context(), database.RemoveUserFromAdminParams{
+		UserID:  params.UserID,
+		GroupID: params.GroupID,
+	}); err != nil {
+		log.Printf("[/api/v1/group/admin/remove]: error removing requested user from admin: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// emit group event REMOVE_ADMIN
+	groupEvent := eventhandlers.GroupEvent{}
+	groupEvent.Name = eventhandlers.REMOVE_ADMIN
+
+	// fetching phonenumbers of groupmembers
+	phonenumbers, err := apiConfig.DB.GetGroupMembersPhonenumbers(r.Context(), params.GroupID)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/remove]: error fetching phonenumbers of group members: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	groupEvent.Phonenumbers = phonenumbers
+
+	// creating group action
+	user, err := apiConfig.DB.GetUserById(r.Context(), params.UserID)
+	if err != nil {
+		log.Printf("[/api/v1/group/admin/remove]: error fetching requested user information: %v", err)
+		utility.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	group := eventhandlers.Group{
+		ID:          params.GroupID,
+		Phonenumber: user.Phonenumber,
+		Username:    user.Username,
+	}
+	groupEvent.Group = group
+
+	// adding notification service
+	groupEvent.NotificationService = apiConfig.NotificationService
+
+	// adding event emitting time
+	groupEvent.EmittedAt = time.Now()
+
+	// emitting event
+	apiConfig.GroupActionsEventEmitterChannel <- groupEvent
+
+	utility.RespondWithJson(w, http.StatusOK, EmptyResponse{
+		AccessToken: newAccessToken,
+	})
 }
