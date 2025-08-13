3d89a41dd9b80852300b7589c20a9563505e1e8a
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-08-14 01:43:01 +0530

    renamed table

---
+++ sql/schema/008_group_message_received.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+create table group_message_received(
+    message_id uuid not null references messages(id) on delete cascade,
+    group_member_id uuid not null references users(id) on delete cascade,
+    group_id uuid not null references groups(id) on delete cascade,
+    received_at timestamp not null,
+    unique(message_id, group_member_id, group_id)
+);
+
+-- +goose Down
+drop table group_message_received;
